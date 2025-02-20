e3bdcb33c076f1d57db980ff4aabdd585dcb573b
Justin900308 <wow3987@gmail.com>
2025-02-20 14:22:58 -0800

    Add files via upload

--- hw7.py
+++ hw7.py
@@ -11,9 +11,9 @@ from scipy import sparse
 ## Global parameters
 k = 1
 n = 4
-N = 2000
-tf = 2
-rf = 13
+N = 10000
+tf = 1
+rf = 1
 dt = tf / N
 A = np.array([[0, 0, 1, 0],
               [0, 0, 0, 1],
@@ -53,7 +53,7 @@ def W_mat(
         B_col: np.ndarray
 ) -> np.ndarray:
     W = np.zeros((n, n))
-    for k in range(N):
+    for k in range(N-1):
         W = W + LA.matrix_power(la.expm(dt * A), N - 1 - k) @ B_col @ B_col.T @ LA.matrix_power(la.expm(dt * A.T),
                                                                                                 N - 1 - k)
     W = W * dt
@@ -68,8 +68,9 @@ x_traj = np.zeros((n, N))
 [A_d, B1_d_col] = dist_sys(A, B1_col)
 B1_d = np.array([B1_d_col[0, 0], B1_d_col[1, 0], B1_d_col[2, 0], B1_d_col[3, 0]])
 W1 = W_mat(A, B1_col)
-
-u1 = np.zeros(N + 1)
+cond_W1= LA.norm(W1,2) * LA.norm(LA.inv(W1),2)
+print('Cond of W:   ',cond_W1)
+u1 = np.zeros(N)
 x = np.zeros((n, N + 1))
 t = np.linspace(0, tf, N + 1)
 for k in range(N):
@@ -84,9 +85,70 @@ plt.ylabel('position')
 plt.legend(['x1', 'x2', 'desired x1'], fontsize=8)
 plt.grid()
 plt.subplot(2, 1, 2)
-plt.plot(t, u1[:])
+plt.plot(t[0:N], u1[:])
 plt.xlabel('time')
 plt.ylabel('control input')
 plt.legend(['u'])
 plt.grid()
+plt.show()
+
+# ## Animation
+# for k in range(N-1):
+#     plt.plot(x[0, k], 0, 'r.')
+#     plt.plot(x[1, k], 0, 'b.')
+#     plt.xlim([-5, 5])
+#     plt.ylim([-0.1, 0.1])
+#     plt.pause(0.0001)
+#     plt.clf()
+# plt.plot(x[0, N], 0, 'r.')
+# plt.plot(x[1, N], 0, 'b.')
+# plt.xlim([-3, 3])
+# plt.ylim([-0.1, 0.1])
+# plt.show()
+
+
+## Case 2
+b1 = 0.99
+b2 = 1
+x_traj = np.zeros((n, N))
+[B1, B1_col] = B_mat(b1, b2)
+[A_d, B1_d_col] = dist_sys(A, B1_col)
+B1_d = np.array([B1_d_col[0, 0], B1_d_col[1, 0], B1_d_col[2, 0], B1_d_col[3, 0]])
+W1 = W_mat(A, B1_col)
+cond_W1= LA.norm(W1,2) * LA.norm(LA.inv(W1),2)
+print('Cond of W:   ',cond_W1)
+u1 = np.zeros(N)
+x = np.zeros((n, N + 1))
+t = np.linspace(0, tf, N + 1)
+for k in range(N):
+    u1[k] = B1_col.T @ la.expm((tf - t[k]) * A.T) @ la.inv(W1) @ x_tf
+    x[:, k + 1] = A_d @ x[:, k] + B1_d * u1[k]
+plt.subplot(2, 1, 1)
+plt.plot(t, x[0, :])
+plt.plot(t, x[1, :])
+plt.plot(tf, x[0, N], 'r.')
+plt.xlabel('time')
+plt.ylabel('position')
+plt.legend(['x1', 'x2', 'desired x1'], fontsize=8)
+plt.grid()
+plt.subplot(2, 1, 2)
+plt.plot(t[0:N], u1[:])
+plt.xlabel('time')
+plt.ylabel('control input')
+plt.legend(['u'])
+plt.grid()
+plt.show()
+
+## Animation
+for k in range(N - 1):
+    plt.plot(x[0, k], 0, 'r.')
+    plt.plot(x[1, k], 0, 'b.')
+    plt.xlim([-5, 5])
+    plt.ylim([-0.1, 0.1])
+    plt.pause(0.0001)
+    plt.clf()
+plt.plot(x[0, N], 0, 'r.')
+plt.plot(x[1, N], 0, 'b.')
+plt.xlim([-3, 3])
+plt.ylim([-0.1, 0.1])
 plt.show()
